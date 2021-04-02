779b99355c423fc7d2c40c019f82312ee4d004de
TasfiaIslam <tasf1a027@gmail.com>
2021-04-02 12:21:55 +0600

    Filter menu items

--- Menu/app.js
+++ Menu/app.js
@@ -75,23 +75,48 @@ const menu = [
 
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
+// load items
 window.addEventListener('DOMContentLoaded', function(){ 
-    let displayMenu = menu.map(function(item){
-      // console.log(item);
-      return `<article class="menu-item">
-                <img src="${item.img}" class="photo" alt="menu item">
-                <div class="item-info">
-                    <header>
-                        <h4>${item.title}</h4>
-                        <h4 class="price">$${item.price}</h4>
-                    </header>
-                    <p class="item-text">
-                        ${item.desc}
-                    </p>
-                </div>
-            </article>`;
-    });
-    displayMenu = displayMenu.join('');
-    sectionCenter.innerHTML = displayMenu;
+    displayMenuItems(menu)
 });
+
+// filter items
+filterBtns.forEach(function(btn){
+   btn.addEventListener('click',function(e){
+     const category = e.currentTarget.dataset.id;
+     const menuCategory = menu.filter(function(menuItem){
+       if (menuItem.category === category){       
+         return menuItem;
+       }
+     });
+
+     if (category === 'all'){
+      displayMenuItems(menu);
+    }
+    else{
+      displayMenuItems(menuCategory);
+    }
+   });
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+    // console.log(item);
+    return `<article class="menu-item">
+              <img src="${item.img}" class="photo" alt="menu item">
+              <div class="item-info">
+                  <header>
+                      <h4>${item.title}</h4>
+                      <h4 class="price">$${item.price}</h4>
+                  </header>
+                  <p class="item-text">
+                      ${item.desc}
+                  </p>
+              </div>
+          </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+}
