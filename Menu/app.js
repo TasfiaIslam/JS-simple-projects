9e6db34e258ce08e511bf598ad6d18c18d3628b1
TasfiaIslam <tasf1a027@gmail.com>
2021-04-02 13:32:15 +0600

    Dynamic buttons

--- Menu/app.js
+++ Menu/app.js
@@ -71,35 +71,27 @@ const menu = [
       img: "./images/item-9.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
+    {
+      id: 10,
+      title: "bff combo",
+      category: "brunch",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
   ];
 
 
 const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn');
+const container = document.querySelector('.btn-container');
+
 
 // load items
 window.addEventListener('DOMContentLoaded', function(){ 
-    displayMenuItems(menu)
+    displayMenuItems(menu);
+    displayMenuButtons();
 });
 
-// filter items
-filterBtns.forEach(function(btn){
-   btn.addEventListener('click',function(e){
-     const category = e.currentTarget.dataset.id;
-     const menuCategory = menu.filter(function(menuItem){
-       if (menuItem.category === category){       
-         return menuItem;
-       }
-     });
-
-     if (category === 'all'){
-      displayMenuItems(menu);
-    }
-    else{
-      displayMenuItems(menuCategory);
-    }
-   });
-});
 
 function displayMenuItems(menuItems){
   let displayMenu = menuItems.map(function(item){
@@ -119,4 +111,37 @@ function displayMenuItems(menuItems){
   });
   displayMenu = displayMenu.join('');
   sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons(){
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    return values;
+  },['all'])
+  const categoryBtns = categories.map(function(category){
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+  }).join('');
+  container.innerHTML = categoryBtns;
+  const filterBtns = container.querySelectorAll('.filter-btn');
+
+  // filter items
+  filterBtns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function(menuItem){
+        if (menuItem.category === category){       
+          return menuItem;
+        }
+      });
+
+      if (category === 'all'){
+      displayMenuItems(menu);
+    }
+    else{
+      displayMenuItems(menuCategory);
+    }
+    });
+  });
 }
